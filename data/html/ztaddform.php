0475fabb9d738abaaff11eb6896eb95a608165df
ChangLeoplod <t.jianlinzhang@gmail.com>
2015-05-12 16:50:17 +0800

    delete data dir

--- data/html/ztaddform.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(!defined('InEmpireCMS'))
-{exit();}
-?><tr bgcolor='#FFFFFF' height=25><td>简介</td><td>
-<textarea name="guanyu" cols="60" rows="10" id="guanyu"><?=$ecmsfirstpost==1?"":ehtmlspecialchars($addr[guanyu])?></textarea>
-</td></tr>
