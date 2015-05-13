734c1ed77ceb08eb8022a9980e53729fe4bb6c4c
ChangLeoplod <t.jianlinzhang@gmail.com>
2015-05-13 17:40:03 +0800

    add pattern for movie collection

--- e/class/userfun.php
+++ e/class/userfun.php
@@ -3,7 +3,7 @@
 function get_imgs($content){
 	$img_arr = array();
 	//$reg = "/.*src=\"([^^]*?)\".*/i";
-	$reg="/<[img|IMG|iframe|IFRAME].*?src=[\'|\"](.*?(?:[\.gif|\.jpg|\.png|\.mp4]))[\'|\"].*?[\/]?>/";
+	$reg="/<[img|IMG|iframe|IFRAME].*?src=[\'|\"](.*?(?:[\.gif|\.jpg|\.png|\.mp4|\.flv|\.avi|\rmvb]))[\'|\"].*?[\/]?>/";
 	preg_match_all($reg, $content, $img_arr);
 	$img_arr = array_unique($img_arr[1]);
 	return $img_arr;
